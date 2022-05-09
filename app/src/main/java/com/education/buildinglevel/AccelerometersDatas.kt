4ea8d29c3de4d5c6cfebd86df9896580c064c509
PC <uladzislau.krukosuki@gmail.com>
2022-05-09 22:17:07 +0300

    In process of create simple level activity

--- app/src/main/java/com/education/buildinglevel/AccelerometersDatas.kt
+++ app/src/main/java/com/education/buildinglevel/AccelerometersDatas.kt
@@ -7,21 +7,40 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.widget.TextView
-import com.education.buildinglevel.databinding.ActivityMainBinding
 
 class AccelerometersDatas(private val activity: Activity, val idName: Int) {
     lateinit var sManager: SensorManager
+    private var magnetic = FloatArray(9)
+    private var gravity = FloatArray(9)
 
-    fun getOptions (){
+    private var accrs = FloatArray(3)
+    private var magf = FloatArray(3)
+    private var values = FloatArray(3)
+
+    fun getOptions() {
         val tvSensor = activity.findViewById<TextView>(idName)
         sManager = activity.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val sListener = object : SensorEventListener{
-            override fun onSensorChanged(sEvent: SensorEvent?) {
-                val value = sEvent?.values
+        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        val sListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                /*val value = sEvent?.values
                 val sData = "Y: ${value?.get(1)}"
-                tvSensor.text = sData
+                tvSensor.text = sData*/
+                when (event?.sensor?.type) {
+                    Sensor.TYPE_ACCELEROMETER->accrs = event.values.clone()
+                    Sensor.TYPE_MAGNETIC_FIELD->magf = event.values.clone()
+                }
+
+                SensorManager.getRotationMatrix(gravity, magnetic, accrs, magf)
+                val outGravity = FloatArray(9)
+                SensorManager.remapCoordinateSystem(gravity,
+                    SensorManager.AXIS_X,
+                    SensorManager.AXIS_Z,
+                    outGravity)
+                SensorManager.getOrientation(outGravity, values)
             }
+
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
@@ -29,6 +48,7 @@ class AccelerometersDatas(private val activity: Activity, val idName: Int) {
 
         }
         sManager.registerListener(sListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sManager.registerListener(sListener, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
 }
