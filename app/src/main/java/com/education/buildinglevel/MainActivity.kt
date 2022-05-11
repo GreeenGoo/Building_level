e3e29be65749f8d07184e08695523e68b0c8e655
PC <uladzislau.krukosuki@gmail.com>
2022-05-11 22:01:31 +0300

    clean all

--- app/src/main/java/com/education/buildinglevel/MainActivity.kt
+++ app/src/main/java/com/education/buildinglevel/MainActivity.kt
@@ -1,32 +1,11 @@
 package com.education.buildinglevel
 
-import android.content.pm.ActivityInfo
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.education.buildinglevel.databinding.ActivityMainBinding
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        supportActionBar?.hide()
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-
-        binding.goToFragment1Button.setOnClickListener{
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_fragment, SimpleLevel())
-                .commit()
-        }
-        binding.goToFragment2Button.setOnClickListener{
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_fragment, ElectronicLevel())
-                .commit()
-        }
+        setContentView(R.layout.activity_main)
     }
 }
