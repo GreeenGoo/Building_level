95537c00684dd9a6f75b8acbaa74eb546c943a81
PC <uladzislau.krukosuki@gmail.com>
2022-06-22 22:28:31 +0300

    Last version (main stick doesn't work properly (to another side).

---
+++ app/src/main/java/com/education/buildinglevel/LandscapeLevel.kt
@@ -0,0 +1,62 @@
+package com.education.buildinglevel
+
+import android.content.Context
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.education.buildinglevel.databinding.LandscapeLevelFragmentBinding
+
+class LandscapeLevel : Fragment() {
+    lateinit var binding: LandscapeLevelFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = LandscapeLevelFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainStickView = binding.mainLevelStick
+        val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val magneticSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+        val accelerometersData = AccelerometersData()
+        val sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                val rData = accelerometersData.getOptions(event)
+                mainStickView.rotation = rData
+                mainStickView.setBackgroundColor(choiceOfColor(rData.toInt()))
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+        }
+        sensorManager.registerListener(
+            sensorEventListener,
+            accelerometerSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            sensorEventListener,
+            magneticSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    private fun choiceOfColor(data: Int): Int {
+        return if (data == 0) {
+            Color.GREEN
+        } else {
+            Color.RED
+        }
+    }
+}
--- app/src/main/java/com/education/buildinglevel/MainActivity.kt
+++ app/src/main/java/com/education/buildinglevel/MainActivity.kt
@@ -6,7 +6,6 @@ import com.education.buildinglevel.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,42 +15,32 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         supportFragmentManager
             .beginTransaction()
-            .replace(R.id.main_fragment, SimpleLevel())
+            .replace(R.id.main_fragment, LandscapeLevel())
             .commit()
-        val bottomNav: BottomNavigationView = binding.bottomNavigation
+        val bottomNavView: BottomNavigationView = binding.bottomNavigation
+        initBottomNavView(bottomNavView)
+    }
+
+    private fun initBottomNavView(bottomNav: BottomNavigationView){
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.landscape_level -> {
                     supportFragmentManager
                         .beginTransaction()
-                        .replace(R.id.main_fragment, SimpleLevel())
+                        .replace(R.id.main_fragment, LandscapeLevel())
                         .commit()
                     return@setOnItemSelectedListener true
                 }
                 R.id.digital_level -> {
                     supportFragmentManager
                         .beginTransaction()
-                        .replace(R.id.main_fragment, ElectronicLevel())
+                        .replace(R.id.main_fragment, DigitalLevel())
                         .commit()
                     return@setOnItemSelectedListener true
                 }
             }
             false
         }
-
-        /*requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-
-        binding.goToFragment1Button.setOnClickListener{
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_fragment, SimpleLevel())
-                .commit()
-        }
-        binding.goToFragment2Button.setOnClickListener{
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_fragment, ElectronicLevel())
-                .commit()
-        }*/
+        bottomNav.setOnItemReselectedListener{}
     }
 }
